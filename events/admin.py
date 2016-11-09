ccd1325ba2162a22da7a05b1feb61c9a7a40242e
Kevin Paul Kalis <kevinpaulkalis@gmail.com>
2016-11-09 19:13:29 +0800

    Add check for Bookmark from DB to prevent duplicates

--- events/admin.py
+++ events/admin.py
@@ -16,9 +16,13 @@ class EventCommentAdmin(admin.ModelAdmin):
     model = EventComment
     list_display = ['user', 'event_title', 'comment', 'comment_date']
 
+class BookmarkAdmin(admin.ModelAdmin):
+    model = Bookmark
+    list_display = ['event_title', 'user', 'active']
+
+admin.site.register(Bookmark, BookmarkAdmin)
 
 admin.site.register(Event, EventAdmin)
 admin.site.register(Participant)
 admin.site.register(Feedback, FeedbackAdmin)
 admin.site.register(EventComment, EventCommentAdmin)
-admin.site.register(Bookmark)
